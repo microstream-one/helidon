dab7ff6d363561acff3a4f7f43c3c25628bbbd32
microstream_hg <h.grunwald@microstream.one>
2021-02-08 12:32:48 +0100

    replaced hard coded storage directory with Junit tempDir

--- microstream/core/src/test/java/io/helidon/microstream/core/ConfigurationTest.java
+++ microstream/core/src/test/java/io/helidon/microstream/core/ConfigurationTest.java
@@ -1,22 +1,45 @@
 package io.helidon.microstream.core;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.StringContainsInOrder.stringContainsInOrder;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import io.helidon.config.ClasspathConfigSource;
 import io.helidon.config.Config;
+import io.helidon.config.ConfigSources;
 import one.microstream.storage.types.EmbeddedStorageManager;
 
 public class ConfigurationTest {
 
+	@TempDir
+	Path tempDir;
+
 	@Test
 	public void createFromConfigTest() {
+
 		Config helidonConfig = Config.builder().addSource(ClasspathConfigSource.create("/microstreamConfig.yml"))
+				.addSource(ConfigSources.create(Map.of("microstream.baseDirectory", tempDir.toString())))
 				.build();
+
 		EmbeddedStorageManagerBuilder embeddedStorageManagerBuilder = EmbeddedStorageManagerBuilder.builder();
 		EmbeddedStorageManager embeddedStorageManager = embeddedStorageManagerBuilder.config(helidonConfig).build();
-	
+
 		assertNotNull(embeddedStorageManager, "embeddedStorageManager is null!");
+
+		//need to compare strings as the microstream APath is not a java path
+		assertThat(tempDir.toString(), stringContainsInOrder(
+				Arrays.asList(embeddedStorageManager.configuration().fileProvider().baseDirectory().toPath())));
+				
+
+		assertThat(embeddedStorageManager.configuration().channelCountProvider().getChannelCount(), is(4));
+		assertThat(embeddedStorageManager.configuration().housekeepingController().housekeepingIntervalMs(), is(2000L));
 	}
 }
