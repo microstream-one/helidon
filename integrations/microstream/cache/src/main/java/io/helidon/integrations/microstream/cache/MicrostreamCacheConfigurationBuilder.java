68166bcf7e064e4c2bfeccc2ecfdfe04130056f8
microstream_hg <h.grunwald@microstream.one>
2021-02-25 14:41:14 +0100

    added microstream jcache

---
+++ integrations/microstream/cache/src/main/java/io/helidon/integrations/microstream/cache/MicrostreamCacheConfigurationBuilder.java
@@ -0,0 +1,154 @@
+package io.helidon.integrations.microstream.cache;
+
+import java.lang.reflect.Field;
+import java.util.function.Predicate;
+
+import javax.cache.configuration.CacheEntryListenerConfiguration;
+import javax.cache.configuration.Factory;
+import javax.cache.expiry.ExpiryPolicy;
+import javax.cache.integration.CacheLoader;
+import javax.cache.integration.CacheWriter;
+
+import io.helidon.config.Config;
+import one.microstream.cache.CacheConfiguration;
+import one.microstream.cache.CacheConfiguration.Builder;
+import one.microstream.cache.CacheConfigurationBuilderConfigurationBased;
+import one.microstream.cache.CacheConfigurationPropertyNames;
+import one.microstream.cache.EvictionManager;
+import one.microstream.configuration.types.Configuration;
+
+public class MicrostreamCacheConfigurationBuilder<K, V> implements CacheConfigurationPropertyNames,
+CacheConfiguration.Builder<K, V>, io.helidon.common.Builder<CacheConfiguration<K, V>> {
+
+	private Builder<K, V> cacheConfigBuilder;
+
+	protected MicrostreamCacheConfigurationBuilder(Class<K> keyType, Class<V> valueType) {
+		super();
+		cacheConfigBuilder = CacheConfiguration.Builder(keyType, valueType);
+	}
+
+	protected MicrostreamCacheConfigurationBuilder(Configuration configuration, Class<K> keyType, Class<V> valueType) {
+		super();
+		cacheConfigBuilder = CacheConfigurationBuilderConfigurationBased.New().buildCacheConfiguration(configuration,
+				CacheConfiguration.Builder(keyType, valueType));
+	}
+
+	/**
+	 * Create a CacheConfiguration builder with default values
+	 *
+	 * @param <K>
+	 * @param <V>
+	 * @param keyType
+	 * @param valueType
+	 * @return
+	 */
+	static <K, V> MicrostreamCacheConfigurationBuilder<K, V> builder(Class<K> keyType, Class<V> valueType) {
+		return new MicrostreamCacheConfigurationBuilder<>(keyType, valueType);
+	}
+
+	/**
+	 * Create a CacheConfiguration builder initialized from the supplied helidon
+	 * configuration node
+	 *
+	 * @param <K>
+	 * @param <V>
+	 * @param config
+	 * @param keyType
+	 * @param valueType
+	 * @return
+	 */
+	static <K, V> MicrostreamCacheConfigurationBuilder<K, V> builder(Config config, Class<K> keyType,
+			Class<V> valueType) {
+		one.microstream.configuration.types.Configuration.Builder configurationBuilder = Configuration.Builder();
+		config.detach().asMap().get().forEach(configurationBuilder::set);
+
+		Configuration configuration = configurationBuilder.buildConfiguration();
+		configuration.opt(KEY_TYPE).ifPresent((s) -> verifyType(s, keyType));
+		configuration.opt(VALUE_TYPE).ifPresent((s) -> verifyType(s, valueType));
+
+		return new MicrostreamCacheConfigurationBuilder<K, V>(configuration, keyType, valueType);
+	}
+
+	@Override
+	public CacheConfiguration<K, V> build() {
+		return cacheConfigBuilder.build();
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> readThrough(boolean readTrough) {
+		cacheConfigBuilder.readThrough(readTrough);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> writeThrough(boolean writeThrough) {
+		cacheConfigBuilder.writeThrough(writeThrough);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> storeByValue(boolean storeByValue) {
+		cacheConfigBuilder.storeByValue(storeByValue);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> enableStatistics(boolean enableStatistics) {
+		cacheConfigBuilder.enableStatistics(enableStatistics);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> enableManagement(boolean enableManagement) {
+		cacheConfigBuilder.enableManagement(enableManagement);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> expiryPolicyFactory(Factory<ExpiryPolicy> expiryPolicyFactory) {
+		cacheConfigBuilder.expiryPolicyFactory(expiryPolicyFactory);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> evictionManagerFactory(
+			Factory<EvictionManager<K, V>> evictionManagerFactory) {
+		cacheConfigBuilder.evictionManagerFactory(evictionManagerFactory);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> cacheLoaderFactory(
+			Factory<CacheLoader<K, V>> cacheLoaderFactory) {
+		cacheConfigBuilder.cacheLoaderFactory(cacheLoaderFactory);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> cacheWriterFactory(
+			Factory<CacheWriter<? super K, ? super V>> cacheWriterFactory) {
+		cacheConfigBuilder.cacheWriterFactory(cacheWriterFactory);
+		return this;
+	}
+
+	@Override
+	public MicrostreamCacheConfigurationBuilder<K, V> serializerFieldPredicate(
+			Predicate<? super Field> serializerFieldPredicate) {
+		cacheConfigBuilder.serializerFieldPredicate(serializerFieldPredicate);
+		return this;
+	}
+
+	@Override
+	public Builder<K, V> addListenerConfiguration(CacheEntryListenerConfiguration<K, V> listenerConfiguration) {
+		cacheConfigBuilder.addListenerConfiguration(listenerConfiguration);
+		return this;
+	}
+
+	private static void verifyType(String typeName, Class<?> expectedType) {
+		if (!typeName.equals(expectedType.getTypeName())) {
+			throw new ConfigException("Microstream cache-config type missmatch, expected: " + expectedType.getTypeName()
+			+ " got: " + typeName);
+		}
+	}
+
+}
---
+++ integrations/microstream/cache/src/test/java/io/helidon/integrations/microstream/cache/ConfigurationTest.java
@@ -0,0 +1,151 @@
+package io.helidon.integrations.microstream.cache;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.typeCompatibleWith;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import javax.cache.configuration.Factory;
+import javax.cache.expiry.ExpiryPolicy;
+import javax.cache.integration.CacheLoader;
+import javax.cache.integration.CacheWriter;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import io.helidon.config.Config;
+import io.helidon.config.ConfigSources;
+import one.microstream.cache.CacheConfiguration;
+import one.microstream.cache.EvictionManager;
+
+public class ConfigurationTest {
+
+	/**
+	 * Test if all default properties are set.
+	 */
+	@Test
+	public void defaultValuesTest() {
+		CacheConfiguration<Integer, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(Integer.class, String.class).build();
+
+		assertAll(() -> assertThat("getKeyType", cacheConfig.getKeyType(), typeCompatibleWith(Integer.class)),
+				() -> assertThat("getValueType", cacheConfig.getValueType(), typeCompatibleWith(String.class)),
+				() -> assertThat("isManagementEnabled", cacheConfig.isManagementEnabled(), is(false)),
+				() -> assertThat("isStatisticsEnabled", cacheConfig.isStatisticsEnabled(), is(false)),
+				() -> assertThat("isReadThrough", cacheConfig.isReadThrough(), is(false)),
+				() -> assertThat("isWriteThrough", cacheConfig.isWriteThrough(), is(false)),
+				() -> assertThat("isStoreByValue", cacheConfig.isStoreByValue(), is(false)),
+				() -> assertThat("getExpiryPolicyFactory", cacheConfig.getExpiryPolicyFactory(),
+						is(CacheConfiguration.DefaultExpiryPolicyFactory())),
+				() -> assertThat("getEvictionManagerFactory", cacheConfig.getEvictionManagerFactory(),
+						is(CacheConfiguration.DefaultEvictionManagerFactory())),
+				() -> assertThat("getCacheLoaderFactory", cacheConfig.getCacheLoaderFactory(), nullValue()),
+				() -> assertThat("getCacheWriterFactory", cacheConfig.getCacheWriterFactory(), nullValue()),
+				() -> assertThat("getCacheEntryListenerConfigurations",
+						cacheConfig.getCacheEntryListenerConfigurations(), emptyIterable()),
+				() -> assertThat("getSerializerFieldPredicate", cacheConfig.getSerializerFieldPredicate(),
+						is(CacheConfiguration.DefaultSerializerFieldPredicate())));
+	}
+
+	/**
+	 * Test if simple configuration values are applied. This test does not check all
+	 * values
+	 */
+	@Test
+	public void configValuesTest() {
+		Map<String, String> source = Map.of("cache.management-enabled", "true", "cache.statistics-enabled", "true",
+				"cache.store-by-value", "true");
+
+		Config config = Config.builder().addSource(ConfigSources.create(source).build()).build();
+
+		CacheConfiguration<Long, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(config.get("cache"), Long.class, String.class).build();
+
+		assertAll(() -> assertThat("getKeyType", cacheConfig.getKeyType(), typeCompatibleWith(Long.class)),
+				() -> assertThat("getValueType", cacheConfig.getValueType(), typeCompatibleWith(String.class)),
+				() -> assertThat("isManagementEnabled", cacheConfig.isManagementEnabled(), is(true)),
+				() -> assertThat("isStatisticsEnabled", cacheConfig.isStatisticsEnabled(), is(true)),
+				() -> assertThat("isStoreByValue", cacheConfig.isStoreByValue(), is(true)));
+	}
+
+	/**
+	 * Test if settings from config can be altered by code
+	 */
+	@Test
+	public void applyChangeTest() {
+
+		Map<String, String> source = Map.of("cache.management-enabled", "true", "cache.statistics-enabled", "true",
+				"cache.store-by-value", "true");
+
+		Config config = Config.builder().addSource(ConfigSources.create(source).build()).build();
+
+		CacheConfiguration<Long, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(config.get("cache"), Long.class, String.class).disableManagement().storeByValue(false).build();
+
+		assertAll(() -> assertThat("isManagementEnabled", cacheConfig.isManagementEnabled(), is(false)),
+				() -> assertThat("isStatisticsEnabled", cacheConfig.isStatisticsEnabled(), is(true)),
+				() -> assertThat("isStoreByValue", cacheConfig.isStoreByValue(), is(false)));
+	}
+
+	@Test
+	public void cacheLoaderFactoryTest() {
+		@SuppressWarnings("unchecked")
+		Factory<CacheLoader<Integer, String>> cacheLoaderFactory = Mockito.mock(Factory.class);
+
+		CacheConfiguration<Integer, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(Integer.class, String.class).cacheLoaderFactory(cacheLoaderFactory).build();
+
+		assertThat(cacheConfig.getCacheLoaderFactory(), sameInstance(cacheLoaderFactory));
+	}
+
+	@Test
+	public void cacheWriterFactoryTest() {
+		@SuppressWarnings("unchecked")
+		Factory<CacheWriter<? super Integer, ? super String>> cacheWriterFactory = Mockito.mock(Factory.class);
+
+		CacheConfiguration<Integer, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(Integer.class, String.class).cacheWriterFactory(cacheWriterFactory).build();
+
+		assertThat(cacheConfig.getCacheWriterFactory(), sameInstance(cacheWriterFactory));
+	}
+
+	@Test
+	public void expiryPolicyFactoryTest() {
+		@SuppressWarnings("unchecked")
+		Factory<ExpiryPolicy> expiryPolicyFactory = Mockito.mock(Factory.class);
+
+		CacheConfiguration<Integer, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(Integer.class, String.class).expiryPolicyFactory(expiryPolicyFactory).build();
+
+		assertThat(cacheConfig.getExpiryPolicyFactory(), sameInstance(expiryPolicyFactory));
+	}
+
+	@Test
+	public void evictionManagerFactoryTest() {
+		@SuppressWarnings("unchecked")
+		Factory<EvictionManager<Integer, String>> evictionManagerFactory = Mockito.mock(Factory.class);
+
+		CacheConfiguration<Integer, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(Integer.class, String.class).evictionManagerFactory(evictionManagerFactory).build();
+
+		assertThat(cacheConfig.getEvictionManagerFactory(), sameInstance(evictionManagerFactory));
+	}
+
+	@Test
+	public void serializerFieldPredicate() {
+		@SuppressWarnings("unchecked")
+		Predicate<? super Field> serializerFieldPredicate = Mockito.mock(Predicate.class);
+
+		CacheConfiguration<Integer, String> cacheConfig = MicrostreamCacheConfigurationBuilder
+				.builder(Integer.class, String.class).serializerFieldPredicate(serializerFieldPredicate).build();
+
+		assertThat(cacheConfig.getSerializerFieldPredicate(), sameInstance(serializerFieldPredicate));
+	}
+}
