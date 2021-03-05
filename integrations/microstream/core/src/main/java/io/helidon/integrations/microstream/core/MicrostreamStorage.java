f43ff244debdeee3b0fb1b351bb2efe820884029
microstream_hg <h.grunwald@microstream.one>
2021-03-05 13:18:50 +0100

    Basic CDI support for microstream

---
+++ integrations/microstream/core/src/main/java/io/helidon/integrations/microstream/core/MicrostreamStorage.java
@@ -0,0 +1,18 @@
+package io.helidon.integrations.microstream.core;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({METHOD, CONSTRUCTOR, FIELD})
+public @interface MicrostreamStorage {
+	String configNode();
+}
