8ec457f2d32b68a1365cfa454bd783977c376972
microstream_hg <h.grunwald@microstream.one>
2021-04-20 13:46:35 +0200

    add an example howto use microstream with Helidon SE

---
+++ examples/integrations/microstream/greetings-se/src/main/java/io/helidon/examples/integrations/microstream/greetings/se/GreetingServiceMicrostreamContext.java
@@ -0,0 +1,62 @@
+package io.helidon.examples.integrations.microstream.greetings.se;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import io.helidon.common.reactive.Single;
+import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+
+public class GreetingServiceMicrostreamContext extends MicrostreamSingleThreadedExecutionContext {
+
+	public GreetingServiceMicrostreamContext(EmbeddedStorageManager storageManager) {
+		super(storageManager);
+	}
+
+	/**
+	 * Add and store a new log entry
+	 *
+	 * @param String
+	 * @return Void
+	 */
+	public CompletableFuture<Void> addLogEntry(String name) {
+		return execute(()->{
+			@SuppressWarnings("unchecked")
+			List<LogEntry> logs = (List<LogEntry>) storageManager().root();
+			logs.add(new LogEntry(name, LocalDateTime.now()));
+			storageManager().store(logs);
+			return null;
+		});
+	}
+
+	/**
+	 * initialize the storage root with a new, empty List
+	 *
+	 * @return Void
+	 */
+	public CompletableFuture<Void> initRootElement() {
+		return execute(()->{
+			if(storageManager().root() == null)
+			{
+				storageManager().setRoot(new ArrayList<LogEntry>());
+				storageManager().storeRoot();
+			}
+			return null;
+		});
+	}
+
+	/**
+	 * returns a List of all stored LogEntries.
+	 *
+	 * @return
+	 */
+	public Single<List<LogEntry>> getLogs() {
+		@SuppressWarnings("unchecked")
+		CompletableFuture<List<LogEntry>> completableFuture = CompletableFuture.supplyAsync(()->{
+			return (List<LogEntry>)storageManager().root();
+		}, executor());
+		return (Single<List<LogEntry>>)Single.create(completableFuture);
+	}
+
+}
---
+++ examples/integrations/microstream/greetings-se/src/main/java/io/helidon/examples/integrations/microstream/greetings/se/LogEntry.java
@@ -0,0 +1,23 @@
+package io.helidon.examples.integrations.microstream.greetings.se;
+
+import java.time.LocalDateTime;
+
+public class LogEntry {
+	private String name;
+	private LocalDateTime dateTime;
+
+	public LogEntry(String name, LocalDateTime dateTime) {
+		super();
+		this.name = name;
+		this.dateTime = dateTime;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+}
