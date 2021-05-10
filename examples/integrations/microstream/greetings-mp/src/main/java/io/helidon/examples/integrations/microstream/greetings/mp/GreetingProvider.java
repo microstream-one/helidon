a2c8901085f17860712bc9838771a9d673f89f29
hg-ms <53219833+hg-ms@users.noreply.github.com>
2021-05-10 13:18:19 +0200

    added a heldion mp example

---
+++ examples/integrations/microstream/greetings-mp/src/main/java/io/helidon/examples/integrations/microstream/greetings/mp/GreetingProvider.java
@@ -0,0 +1,58 @@
+package io.helidon.examples.integrations.microstream.greetings.mp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import io.helidon.integrations.microstream.core.MicrostreamStorage;
+import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+
+@ApplicationScoped
+public class GreetingProvider  {
+
+	private EmbeddedStorageManager storage;
+	private List<String> greetingMessages;
+	private Random rnd = new Random();
+
+	private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+
+	@SuppressWarnings("unchecked")
+	@Inject
+	public GreetingProvider(@MicrostreamStorage(configNode = "one.microstream.storage.greetings")EmbeddedStorageManager storage) {
+		super();
+		this.storage = storage;
+
+		//Initialize storage if empty or load greetings
+		if((greetingMessages = (List<String>)storage.root()) == null)
+		{
+			greetingMessages = new ArrayList<String>();
+			storage.setRoot(greetingMessages);
+			storage.storeRoot();
+			addGreeting("Hello");
+		}
+	}
+
+	public void addGreeting(String newGreeting) {
+		try {
+			lock.writeLock().lock();
+			greetingMessages.add(newGreeting);
+			storage.store(greetingMessages);
+		} finally {
+			lock.writeLock().unlock();
+		}
+	}
+
+	public Object getGreeting() {
+		try {
+			lock.readLock().lock();
+			return greetingMessages.get(rnd.nextInt(greetingMessages.size()));
+		} finally {
+			lock.readLock().unlock();
+		}
+	}
+
+}
