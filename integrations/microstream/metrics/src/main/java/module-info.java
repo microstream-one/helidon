49bd355907fe2b3793d9dcc11d97aeab187e4857
hg-ms <53219833+hg-ms@users.noreply.github.com>
2021-05-21 11:50:25 +0200

    module names fixed

--- integrations/microstream/metrics/src/main/java/module-info.java
+++ integrations/microstream/metrics/src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module io.helidon.integrations.microstream {
-	exports io.helidon.integrations.microstream.metrics;
+module io.helidon.integrations.microstream.metrics {
+    exports io.helidon.integrations.microstream.metrics;
 
-	requires transitive io.helidon.common;
-	requires transitive io.helidon.config;
-	requires transitive io.helidon.metrics;
-	requires transitive microstream.storage.embedded;
+    requires transitive io.helidon.common;
+    requires transitive io.helidon.config;
+    requires transitive io.helidon.metrics;
+    requires transitive microstream.storage.embedded;
 }
